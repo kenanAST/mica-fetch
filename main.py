55db47a6ad5cbbb790c2d227a4a2dacde264f4a1
kenanAST <kenanjoshua3454@gmail.com>
2024-03-22 22:44:51 +0800

    chore: added function for fetching

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import requests
 from get_token import get_token
 import json
-###Changes every 5 minutes
-
+import threading
+from datetime import datetime
 
 def write_file(filename, data):
     f = open(filename, 'w')
@@ -17,16 +17,25 @@ def main():
         "Authorization" : token
     }
 
+
+    def fetch_data(idnum):
+
+        getStudentResponse = requests.get(f'https://micaapi.msuiit.edu.ph/api/info/v2/student/get_info?studid={idnum}', headers=headers)
+        getProspectusResponse = requests.get(f'https://micaapi.msuiit.edu.ph/api/info/v1/student/view/prospectus?studid={idnum}', headers=headers)
+
+
+        write_file(f'prospectus/{idnum.split("-")[0]}/{idnum}.json', getProspectusResponse.text)
+        write_file(f'student_info/{idnum.split("-")[0]}/{idnum}.json', getStudentResponse.text)
+
+        return [getStudentResponse, getProspectusResponse]
+
     for i in range(2019, 2020):
         for j in range(500, 520):
             idnum = f"{str(i).zfill(4)}-{str(j).zfill(4)}"
 
-            getStudentResponse = requests.get(f'https://micaapi.msuiit.edu.ph/api/info/v2/student/get_info?studid={idnum}', headers=headers)
-            getProspectusResponse = requests.get(f'https://micaapi.msuiit.edu.ph/api/info/v1/student/view/prospectus?studid={idnum}', headers=headers)
 
+            getStudentResponse, getProspectusResponse = fetch_data(idnum)
 
-            write_file(f'prospectus/{idnum.split("-")[0]}/{idnum}.json', getProspectusResponse.text)
-            write_file(f'student_info/{idnum.split("-")[0]}/{idnum}.json', getStudentResponse.text)
 
             student_info = json.loads(getStudentResponse.text)
             prospectus = json.loads(getProspectusResponse.text)
@@ -36,6 +45,8 @@ def main():
                     headers = {
                         "Authorization": get_token()
                     }
+
+                    fetch_data(idnum)
             except:
                 pass
     
