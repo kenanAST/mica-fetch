77606cdc54b22c8171d509b015bca50a938a362e
kenanAST <kenanjoshua3454@gmail.com>
2024-03-22 22:47:45 +0800

    token checker setup

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ def main():
     }
 
 
-    def fetch_data(idnum):
+    def fetch_data(idnum, TokenChecker):
 
         getStudentResponse = requests.get(f'https://micaapi.msuiit.edu.ph/api/info/v2/student/get_info?studid={idnum}', headers=headers)
         getProspectusResponse = requests.get(f'https://micaapi.msuiit.edu.ph/api/info/v1/student/view/prospectus?studid={idnum}', headers=headers)
@@ -27,21 +27,26 @@ def main():
         write_file(f'prospectus/{idnum.split("-")[0]}/{idnum}.json', getProspectusResponse.text)
         write_file(f'student_info/{idnum.split("-")[0]}/{idnum}.json', getStudentResponse.text)
 
+        TokenChecker.append({"responses" : {[getStudentResponse, getProspectusResponse]}, "timestamp" : datetime.now().strftime("%d/%m/%Y %H:%M:%S") })        
         return [getStudentResponse, getProspectusResponse]
 
     for i in range(2019, 2020):
         for j in range(500, 520):
             idnum = f"{str(i).zfill(4)}-{str(j).zfill(4)}"
 
+            TokenChecker = []
 
             getStudentResponse, getProspectusResponse = fetch_data(idnum)
 
+            getStudentResponse, getProspectusResponse = TokenChecker[len(TokenChecker) - 1]['responses'] 
+            timestamp = TokenChecker[len(TokenChecker) - 1]['timestamp']
 
             student_info = json.loads(getStudentResponse.text)
             prospectus = json.loads(getProspectusResponse.text)
 
             try: 
                 if(student_info['message'] == 'Token is invalid' or prospectus['message' == 'Token is invalid']):
+                    print(f"Token Change @ {timestamp}")
                     headers = {
                         "Authorization": get_token()
                     }
