bd3e62a583890e4039f75de5d2343c6b5bae3e57
kenanAST <kenanjoshua3454@gmail.com>
2024-03-22 18:32:35 +0800

    feat: added token fetch

--- helpers.py
+++ helpers.py
@@ -4,28 +4,31 @@ from selenium.webdriver.common.by import By
 from dotenv import load_dotenv
 import os
 
-from time import sleep
-
 load_dotenv()
 
-url = 'https://mica.msuiit.edu.ph/'
 
-driver = webdriver.Chrome()
-driver.get(url)
+def get_token():
+    url = 'https://mica.msuiit.edu.ph/'
+    driver = webdriver.Chrome()
+    driver.get(url)
 
-username_xpath = '/html/body/div[1]/main/div[1]/div/div/div/div[1]/div/div[1]/div/form/div[1]/input'
-password_xpath = '/html/body/div[1]/main/div[1]/div/div/div/div[1]/div/div[1]/div/form/div[2]/input'
+    username_xpath = '/html/body/div[1]/main/div[1]/div/div/div/div[1]/div/div[1]/div/form/div[1]/input'
+    password_xpath = '/html/body/div[1]/main/div[1]/div/div/div/div[1]/div/div[1]/div/form/div[2]/input'
 
-username = driver.find_element(By.XPATH, username_xpath)
-username_details = os.getenv('USERNAME')
-username.send_keys(username_details)
+    username = driver.find_element(By.XPATH, username_xpath)
+    username_details = os.getenv('USERNAME')
+    username.send_keys(username_details)
 
-password = driver.find_element(By.XPATH, password_xpath)
-password_details = os.getenv('PASSWORD')
-password.send_keys(password_details)
-password.send_keys(Keys.ENTER)
+    password = driver.find_element(By.XPATH, password_xpath)
+    password_details = os.getenv('PASSWORD')
+    password.send_keys(password_details)
+    password.send_keys(Keys.ENTER)
 
-sleep(3)
+    token = driver.get_cookie('token')['value']
 
+    print(f"Token: {token}")
+    print(f"Data Type: {type(token)}")
+
+    return token
 
 
--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 import requests
-
-
+from helpers import get_token
 ###Changes every 5 minutes
 headers = {
-    "Authorization" : "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Impvc2h1YWtlbmFuLmNpbmNoZXNAZy5tc3VpaXQuZWR1LnBoIiwiaWF0IjoxNzExMDk3OTI2LCJleHAiOjE3MTEwOTk3MjZ9.MhwP1YChu714PqneNNDeEbdg7s0R8gVh5ZWrOdOhYl0"
+    "Authorization" : get_token()
 }
 
 getStudentResponse = requests.get('https://micaapi.msuiit.edu.ph/api/info/v2/student/get_info?studid=2019-8904', headers=headers)
